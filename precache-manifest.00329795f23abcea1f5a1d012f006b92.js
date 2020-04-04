9e1fbe34591e178037cac4cedab480a166e309f3
Fettoukh <medamine.fettoukh@gmail.com>
2020-04-04 19:07:27 +0200

    Updates

---
+++ precache-manifest.00329795f23abcea1f5a1d012f006b92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83070fbe344ca6e30f1d2975421c4d5",
+    "url": "/React-Movies-IMBD-API/index.html"
+  },
+  {
+    "revision": "6e6f1ce9707103e55f78",
+    "url": "/React-Movies-IMBD-API/static/css/main.01690f44.chunk.css"
+  },
+  {
+    "revision": "4cfc27e8653aa031e006",
+    "url": "/React-Movies-IMBD-API/static/js/2.79735fc8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-Movies-IMBD-API/static/js/2.79735fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6f1ce9707103e55f78",
+    "url": "/React-Movies-IMBD-API/static/js/main.b3491455.chunk.js"
+  },
+  {
+    "revision": "b1a1eee9b11dd1c3cb65",
+    "url": "/React-Movies-IMBD-API/static/js/runtime-main.a913e711.js"
+  },
+  {
+    "revision": "584b607f5c2ff075429dc0e7b8d142ef",
+    "url": "/React-Movies-IMBD-API/static/media/gif2.584b607f.gif"
+  }
+]);
